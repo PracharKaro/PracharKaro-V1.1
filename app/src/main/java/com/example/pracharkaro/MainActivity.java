8fe2b918cdb261d3dd6feeed792040f7d678d3d8
kunaljha1234 <kunaljha004@gmail.com>
2021-12-01 15:07:23 +0530

    register page commited

--- app/src/main/java/com/example/pracharkaro/MainActivity.java
+++ app/src/main/java/com/example/pracharkaro/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.pracharkaro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new Login());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
